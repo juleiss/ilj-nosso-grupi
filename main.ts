1ab4911fc8139745d96e74d8e191d6e5ca41214b
juleiss <114228324+juleiss@users.noreply.github.com>
2022-09-23 08:01:09 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("Bora galerinha!")
+    basic.clearScreen()
+    basic.pause(randint(1000, 5000))
+    basic.showIcon(IconNames.Giraffe)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # #
+        . . # # #
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+})
+basic.forever(function () {
+	
+})
