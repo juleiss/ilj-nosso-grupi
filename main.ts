925ba63320abec7d78cbf1fd24c21a5e36b0cf07
juleiss <114228324+juleiss@users.noreply.github.com>
2022-09-30 08:00:54 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+let tocar = false
+let p2 = 0
+let p1 = 0
 input.onPinPressed(TouchPin.P0, function () {
+    tocar = false
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
-    basic.showString("Bora galerinha!")
+    basic.showString("Bora winx")
     basic.clearScreen()
     basic.pause(randint(1000, 5000))
     basic.showIcon(IconNames.Giraffe)
@@ -10,22 +14,30 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.clearScreen()
 })
 input.onPinPressed(TouchPin.P2, function () {
-    basic.showLeds(`
-        . . . # #
-        . . . # #
-        . . . # #
-        . . # # #
-        . . # # #
-        `)
+    if (tocar == false) {
+        tocar = true
+        p2 += 1
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . # # #
+            . . # # #
+            `)
+    }
 })
 input.onPinPressed(TouchPin.P1, function () {
-    basic.showLeds(`
-        # # # . .
-        # # # . .
-        # # # . .
-        # # . . .
-        # # . . .
-        `)
+    if (tocar == false) {
+        tocar = true
+        p1 += 1
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            # # . . .
+            # # . . .
+            `)
+    }
 })
 basic.forever(function () {
 	
